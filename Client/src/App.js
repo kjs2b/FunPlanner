12df8b9037d5c4630476ffaa9104e8d04d96bf6c
Kevin <kjs2b@virginia.edu>
2017-04-20 19:56:55 -0700

    Added methods back into App class Navigator reimplementation

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, NavigatorIOS } from 'react-native';
 import Login from './components/Login';
-//import { StackNavigator } from 'react-navigation';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { loggedIn: false };
+    this.login = this.logIn.bind(this);
+  }
+
+  logIn() {
+    this.setState({ loggedIn: true });
+  }
+
   render() {
     return (
       <NavigatorIOS
@@ -14,33 +24,5 @@ class App extends Component {
       )
   }
 }
-
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = { loggedIn: false };
-//     this.login = this.logIn.bind(this);
-//   }
-
-//   logIn() {
-//     this.setState({ loggedIn: true });
-//   }
-
-//   render() {
-//     if (this.state.loggedIn !== null) {
-//       if (!this.state.loggedIn) {
-//         return (
-//           <Login onLogin={this.logIn} />
-//         );
-//       }
-//         return (
-//           <View>
-//             <Checker />
-//           </View>
-//         );
-//     }
-//   }
-// }
 
 export default App;
